20201382e6969c62b7c0191a39788fb032bb4b57
Thomas Olaya Marulanda <thomas.olayam@uqvirtual.edu.co>
2026-09-24 15:31:36 -0500

    completo la clase Cliente -Thomas

--- .idea/Main/Cliente.java
+++ .idea/Main/Cliente.java
@@ -1,2 +1,78 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Cliente {
+
+    private String nombre;
+    private int documento;
+    private int telefono;
+    private String correo;
+
+    //creacion de lista
+    private List<Compra> listaCompras;
+
+    public Cliente(String nombre, int documento, int telefono, String correo) {
+        this.nombre = nombre;
+        this.documento = documento;
+        this.telefono = telefono;
+        this.correo = correo;
+
+        //inicializo la lista
+        listaCompras= new ArrayList<>();
+    }
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(int documento) {
+        this.documento = documento;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public List<Compra> getListaCompras() {
+        return listaCompras;
+    }
+
+    public void setListaCompras(List<Compra> listaCompras) {
+        this.listaCompras = listaCompras;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nombre:'" + nombre + '\'' +
+                ", documento:" + documento +
+                ", telefono:" + telefono +
+                ", correo:'" + correo + '\'' +
+                ", listaCompras:" + listaCompras +
+                '}';
+    }
 }
