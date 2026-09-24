5132aecb3b9825fce5ac8c5e1546b025b1bd7940
Thomas Olaya Marulanda <thomas.olayam@uqvirtual.edu.co>
2026-09-24 15:15:15 -0500

    completo la clase compra -Thomas

--- .idea/Main/Compra.java
+++ .idea/Main/Compra.java
@@ -1,5 +1,74 @@
+import java.time.LocalDate;
+
 public class Compra {
 
 
     private String codigo;
+    private LocalDate fechaRealizacion;
+    private Double valorTotal;
+
+    //relacion Compra con MetodoPago
+    private MetodoPago metodoPago;
+
+    //relacion Compra con Cliente
+    private Cliente cliente;
+
+    public Compra(String codigo, LocalDate fechaRealizacion, Double valorTotal, MetodoPago metodoPago, Cliente cliente) {
+        this.codigo = codigo;
+        this.fechaRealizacion = fechaRealizacion;
+        this.valorTotal = valorTotal;
+        this.metodoPago = metodoPago;
+        this.cliente = cliente;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDate getFechaRealizacion() {
+        return fechaRealizacion;
+    }
+
+    public void setFechaRealizacion(LocalDate fechaRealizacion) {
+        this.fechaRealizacion = fechaRealizacion;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public MetodoPago getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(MetodoPago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra{" +
+                "codigo='" + codigo + '\'' +
+                ", fechaRealizacion=" + fechaRealizacion +
+                ", valorTotal=" + valorTotal +
+                ", metodoPago=" + metodoPago +
+                ", cliente=" + cliente +
+                '}';
+    }
 }
