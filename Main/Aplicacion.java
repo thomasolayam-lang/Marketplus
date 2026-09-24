af99a34f307a6264db720d71807096166f45926d
juanamarinl-beep <juana.marinl@uqvirtual.edu.co>
2026-09-24 16:03:03 -0500

    Básico en aplicación - Juan

--- Main/Aplicacion.java
+++ Main/Aplicacion.java
@@ -1,3 +1,13 @@
-public class Aplicacion {
+import java.util.Scanner;
 
+public class Aplicacion {
+public static void main (String[] args){
+
+    //Menú interactivo
+
+    Scanner sc=new Scanner(System.in);
+
+    Supermercado supermercado= new Supermercado("MarketPlus", "Carrera 25", 312763872  );
+
+}
 }
